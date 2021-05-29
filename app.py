167f89b632f01fdcb12ab6304fdc0d4062b038e0
lucamadd <44671856+lucamadd@users.noreply.github.com>
2021-05-29 12:51:58 +0200

    Added song.html

---
+++ app.py
@@ -0,0 +1,14 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myclient["local"]
+
+collist = mydb.list_collection_names()
+if "provatrack" in collist:
+  print("The collection exists.")
+
+mycol = mydb["provatrack"]
+
+for x in mycol.find():
+  print(x)
